abf35cd86f473dcb0341c56258ec052c563fafa4
Daniel-Ovelar <126206750+Daniel-Ovelar@users.noreply.github.com>
2023-05-22 17:41:25 -0300

    agregue un archivo conversionGrados.js

---
+++ conversionGrados.js
@@ -0,0 +1,10 @@
+// celsius = (fahrenheit - 32 ) * 5/9
+
+let f1 = 50 ;
+let f2 = 75 ;
+
+let centigrados1 = ( (f1 - 32) * 5/9  );
+let centigrados2 = ( (f2 - 32) * 5/9  );
+
+console.log(centigrados1);
+console.log(centigrados2);
