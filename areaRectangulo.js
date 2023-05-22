dcb4391226a66a7f2b9346631aeed42adc754f39
Daniel-Ovelar <126206750+Daniel-Ovelar@users.noreply.github.com>
2023-05-22 17:39:26 -0300

    agregue un archivo areaRectangulo.js

---
+++ areaRectangulo.js
@@ -0,0 +1,11 @@
+/*   Area = Base * Altura   */
+let Base = 5 ;
+let Altura = 3 ;
+let Area = ( Base * Altura);
+console.log( Area);
+
+let Base2 = 12.5 ;
+let Altura2 = 5.3 ;
+let Area2 = (Base2 * Altura2);
+console.log (Area2);
+
